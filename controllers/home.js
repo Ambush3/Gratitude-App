c416e25ffe443fbef69a3431b020f7400fcbd9a2
aaronbush3 <aaronbush3@gmail.com>
2022-10-09 21:44:59 -0400

    Started on daily motivation page

--- controllers/home.js
+++ controllers/home.js
@@ -2,4 +2,8 @@ module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs");
   },
+
+    getMotivation: (req, res) => {
+    res.render("daily-motivation.ejs");
+  }
 };
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const { ensureAuth} = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
@@ -16,5 +16,6 @@ router.post("/signup", authController.postSignup);
 router.get("/forgot-password", authController.getForgotPassword);
 router.post("/forgot-password", authController.postForgotPassword);
 router.delete("/post/:id", postsController.deletePost);
+router.get("/daily-motivation", homeController.getMotivation);
 
 module.exports = router;
