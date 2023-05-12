e81eb9cad823de9a6367a5d6de162d45350b58fa
aaronbush3 <aaronbush3@gmail.com>
2023-05-11 22:00:48 -0400

    Add a cache so that when we refresh the page the quote stays the same for a day

--- controllers/motivation.js
+++ controllers/motivation.js
@@ -1,33 +1,33 @@
 const https = require('https');
 
+let cachedQuote;
+let cachedDate;
+
 module.exports = {
     getMotivation: (req, res) => {
-        // get api data from https://zenquotes.io/api/quotes/happiness
-        // use in daily-motivation.ejs
-        // res.render('daily-motivation', {motivation: motivation});
-        https.get('https://zenquotes.io/api/quotes/happiness', (resp) => {
-            let data = '';
-            // A chunk of data has been received.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                // render into daily-motivation.ejs into <p><%= quote.h %></p> and <p><%= quote.a %></p>
-                res.render('daily-motivation', {quote: JSON.parse(data)[0]});
+        // check if quote and date are already in cache
+        const now = new Date();
+        if (cachedQuote && cachedDate && cachedDate.getDate() === now.getDate()) {
+            // if they are, render the cached quote
+            res.render('daily-motivation', { quote: cachedQuote });
+        } else {
+            https.get('https://zenquotes.io/api/quotes/happiness', (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    // update cache with new quote and date
+                    const newQuote = JSON.parse(data)[0];
+                    cachedQuote = newQuote;
+                    cachedDate = now;
+
+                    // render the new quote
+                    res.render('daily-motivation', { quote: newQuote });
+                });
+            }).on('error', (err) => {
+                console.log('Error: ' + err.message);
             });
         }
-        ).on("error", (err) => {
-
-            console.log("Error: " + err.message);
-
-        // the response format
-        // q = quote text
-        // a = author name
-        // i = author image (key required)
-        // c = character count
-        // h = pre-formatted HTML quote
-
-    });
-    }
-}
+    },
+};
